3736946d568ea6deb41d16612835c16224aefce1
mecforlove <mecforlove@outlook.com>
2017-10-11 00:18:16 +0800

    add requirements;add some classes

--- yunsdk/__init__.py
+++ yunsdk/__init__.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""Python client library for the PCS API.
+"""
+from requests import Session
+from . import version
+
+__version__ = version.__version__
 
 
 class YunApi(object):
-    def __init__(self):
+    """A client for the PCS API.
+    """
+    def __init__(self, bduss, session=None, timeout=None):
+        self.bduss = bduss
+        self.session = session or Session()
+        self.timeout = timeout
+
+    def request(self, uri):
         pass
+
+
+class YunApiError(Exception):
+    """YunApiError
+    """
+    pass
---
+++ yunsdk/version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""version of the sdk
+"""
+
+__version__ = "1.0.0"
